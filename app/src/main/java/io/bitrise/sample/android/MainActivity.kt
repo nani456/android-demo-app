6574cfd53f0b952b10ab1a15a2db84275f58fbcd
nani456 <nagini.kandula@gmail.com>
2025-10-21 16:12:47 +0530

    Update MainActivity.kt

--- app/src/main/java/io/bitrise/sample/android/MainActivity.kt
+++ app/src/main/java/io/bitrise/sample/android/MainActivity.kt
@@ -2,12 +2,10 @@ package io.bitrise.sample.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(MainActivity.this,"testing bitrise",Toast.LENGTH_LONG).show();
     }
 }
